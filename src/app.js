a1a94acc3249b656e97bcd05bef0dfde690b2225
StevenNagata <nagata.steven1@gmail.com>
2018-10-02 13:33:57 -0700

    In App.js set initial state to have an array of flashcards equal to the empty array. #1

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ import React from 'react'
 import FlashcardForm from './flashcard-form'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      flashcards: []
+    }
+  }
 
   render() {
     return (
