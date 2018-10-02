3a58d1eda7083ad7fd982a487d19af4d79f2db01
StevenNagata <nagata.steven1@gmail.com>
2018-10-02 14:32:30 -0700

    Added an onSubmit event to the save button in flashcard-form.js to call a method to append the question and answer of the current flashcard to the app.state flashcards array (as an object). #1

--- src/app.js
+++ src/app.js
@@ -7,11 +7,22 @@ export default class App extends React.Component {
     this.state = {
       flashcards: []
     }
+    this.saveFlashcard = this.saveFlashcard.bind(this)
+  }
+
+  saveFlashcard(question, answer) {
+    const flashcards = this.state.flashcards.map((card) =>
+      Object.assign({}, card)
+    )
+    const newCard = { question, answer }
+    flashcards.push(newCard)
+    this.setState({ flashcards })
   }
 
   render() {
+    const { saveFlashcard } = this
     return (
-      <FlashcardForm />
+      <FlashcardForm saveFlashcard={saveFlashcard} />
     )
   }
 }
--- src/flashcard-form.js
+++ src/flashcard-form.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
 export default class FlashcardForm extends React.Component {
-
+  constructor(props) {
+    super(props)
+    this.save = this.save.bind(this)
+  }
+  save() {
+    let currentQuestion = document.getElementById('currentQuestion')
+    let currentAnswer = document.getElementById('currentAnswer')
+    this.props.saveFlashcard(currentQuestion.value, currentAnswer.value)
+    currentQuestion.value = ''
+    currentAnswer.value = ''
+  }
   render() {
     return (
       <div className="container-fluid w-50 border border-dark p-4 rounded">
@@ -13,11 +23,11 @@ export default class FlashcardForm extends React.Component {
           </div>
           <div className="form-group">
             <label htmlFor="formGroupExampleInput2">Answer</label>
-            <input type="text" className="form-control" id="currentAnwser" placeholder="Enter answer" />
+            <input type="text" className="form-control" id="currentAnswer" placeholder="Enter answer" />
           </div>
         </form>
         <div className="d-flex justify-content-center p-2">
-          <button type="button" className="btn btn-outline-dark">Save</button>
+          <button onClick={this.save} type="button" className="btn btn-outline-dark">Save</button>
         </div>
       </div>
     )
