ad7d9c4f166608d7235d57e17250fa35e0de2eea
StevenNagata <nagata.steven1@gmail.com>
2018-10-03 14:15:38 -0700

    Delete redundant nested divs in my-flashcards.js #3

--- src/my-flashcards.js
+++ src/my-flashcards.js
@@ -11,21 +11,19 @@ export default class MyFlashcards extends React.Component {
     else {
       return (
         <div className="container p-2">
-          <div className="container">
-            <div className="row d-flex justify-content-center">
-              {
-                this.props.flashcards.map((card, index) => {
-                  return (
-                    <div key={index} className="card bg-light mb-3 m-3 w-25">
-                      <div className="card-body">
-                        <p className="card-text m-1">Q: {card.question}</p>
-                        <p className="card-text m-1">A: {card.answer}</p>
-                      </div>
+          <div className="row d-flex justify-content-center">
+            {
+              this.props.flashcards.map((card, index) => {
+                return (
+                  <div key={index} className="card bg-light mb-3 m-3 w-25">
+                    <div className="card-body">
+                      <p className="card-text m-1">Q: {card.question}</p>
+                      <p className="card-text m-1">A: {card.answer}</p>
                     </div>
-                  )
-                })
-              }
-            </div>
+                  </div>
+                )
+              })
+            }
           </div>
         </div>
       )
