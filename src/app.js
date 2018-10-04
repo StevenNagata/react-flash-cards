9dfaeb1bcbafb1da4dce5be4f580b5056542848a
StevenNagata <nagata.steven1@gmail.com>
2018-10-03 23:13:34 -0700

    Added params to appstate so the app knows which card to edit/ created a new method to save the updated flashcards after editing #5

--- src/app.js
+++ src/app.js
@@ -11,12 +11,23 @@ export default class App extends React.Component {
     const stateJson = localStorage.getItem('current-app-state')
     const appState = JSON.parse(stateJson) || {}
     const path = hash.parse(location.hash).path
+    const params = hash.parse(location.hash).params
     this.state = {
-      view: { path },
+      view: {
+        path: path,
+        params: params
+      },
       flashcards: appState.flashcards || []
     }
     this.saveFlashcard = this.saveFlashcard.bind(this)
     this.takeToForm = this.takeToForm.bind(this)
+    this.saveEditedFlashcards = this.saveEditedFlashcards.bind(this)
+  }
+  saveEditedFlashcards(flashcards) {
+    this.setState({
+      flashcards,
+      view: { path: 'view' }
+    })
   }
   takeToForm() {
     this.setState({
@@ -24,26 +35,26 @@ export default class App extends React.Component {
     })
   }
   renderView() {
-    const { path } = this.state.view
+    const { path, params } = this.state.view
     switch (path) {
       case 'create':
         return <FlashcardForm saveFlashcard={this.saveFlashcard} />
       case 'edit':
-        return <EditFlashcard flashcards={this.state.flashcards} />
+        return <EditFlashcard flashcards={this.state.flashcards} saveEditedFlashcards={this.saveEditedFlashcards} params={params} />
       default:
-        return <MyFlashcards flashcards={this.state.flashcards} takeToForm={this.takeToForm} />
+        return <MyFlashcards flashcards={this.state.flashcards} takeToForm={this.takeToForm} editCard={this.editCard} />
     }
   }
   componentDidMount() {
     window.addEventListener('hashchange', () => {
-      const { path } = hash.parse(location.hash)
+      const { path, params } = hash.parse(location.hash)
       this.setState({
-        view: { path }
+        view: { path, params }
       })
     })
     window.addEventListener('beforeunload', () => {
-      const { flashcards } = this.state
-      const stateJson = JSON.stringify({ flashcards })
+      const { view, flashcards } = this.state
+      const stateJson = JSON.stringify({ view, flashcards })
       localStorage.setItem('current-app-state', stateJson)
     })
   }
