f1d0bb1bfd49f013afd89f32dcd2d474f2fa4619
StevenNagata <nagata.steven1@gmail.com>
2018-10-04 09:27:38 -0700

    updated theme and edit symbol to be more visually appealing #5

--- src/my-flashcards.js
+++ src/my-flashcards.js
@@ -2,15 +2,6 @@ import React from 'react'
 import NoFlashcards from './no-flashcards'
 
 export default class MyFlashcards extends React.Component {
-  // constructor(props) {
-  //   super(props)
-  //   this.editIndex = this.editIndex.bind(this)
-  // }
-  // editIndex(event) {
-  //   const editCard = event.target.closest('#complete-flashcard')
-  //   const editCardIndex = editCard.getAttribute('index')
-  //   this.props.editCard(editCardIndex)
-  // }
   render() {
     if (this.props.flashcards.length === 0) {
       return (
@@ -18,7 +9,6 @@ export default class MyFlashcards extends React.Component {
       )
     }
     else {
-
       return (
         <div className="container p-2">
           <div className="row d-flex justify-content-center">
@@ -30,7 +20,7 @@ export default class MyFlashcards extends React.Component {
                     <div className="card-body">
                       <p className="card-text m-1">Q: {card.question}</p>
                       <p className="card-text m-1">A: {card.answer}</p>
-                      <a className="float-right" href={href}>&#9997;</a>
+                      <a className="float-right" href={href}>&#9999;</a>
                     </div>
                   </div>
                 )
