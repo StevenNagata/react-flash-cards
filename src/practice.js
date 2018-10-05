b7879796f586e365cde11e653f3b14307da0b0bd
StevenNagata <nagata.steven1@gmail.com>
2018-10-04 21:24:31 -0700

    Call the practice component in app.js in render view method and pass the flashcards as a prop #7

---
+++ src/practice.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class Practice extends React.Component {
+
+  render() {
+    return (
+      <div className="w-75 container d-flex">
+        <div className="jumbotron">
+          <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <a className="btn btn-primary btn-sm m-3" role="button">Show answer</a>
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        </div>
+      </div>
+    )
+  }
+}
